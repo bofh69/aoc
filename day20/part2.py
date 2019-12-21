3c7bc00c3f7d5e2797e2f6f8dcecff4ce9d2ef55
Sebastian Andersson <sebastian@bittr.nu>
2019-12-21 10:18:10 +0100

    The solution

--- day20/part2.py
+++ day20/part2.py
@@ -45,9 +45,9 @@ def neighbor(nodes, pos):
 def in_or_out(pos, min_x, max_x, min_y, max_y):
     (x, y) = pos
     if x < min_x or x > max_x or y < min_y or y > max_y:
-        return 'I'
-    else:
         return 'O'
+    else:
+        return 'I'
 
 def parse_file(file):
     inp = read_file(file)
@@ -71,13 +71,13 @@ def parse_file(file):
                 add_portal(rows, (x+1, y), (x+1, y), (x+2, y), portals)
     for pos, node in nodes.items():
         if north(pos) in nodes:
-            node.append((north(pos), 'N'))
+            node.append((north(pos), 1))
         if south(pos) in nodes:
-            node.append((south(pos), 'S'))
+            node.append((south(pos), 1))
         if east(pos) in nodes:
-            node.append((east(pos), 'E'))
+            node.append((east(pos), 1))
         if west(pos) in nodes:
-            node.append((west(pos), 'W'))
+            node.append((west(pos), 1))
             
     min_x = 100000
     min_y = 100000
@@ -93,59 +93,74 @@ def parse_file(file):
             max_x = x
         if y > max_y:
             max_y = y
-    for name, pos in portals[1].items():
-        if name != 'AA' and name != 'ZZ':
-            n1 = neighbor(nodes, pos[0])
-            n2 = neighbor(nodes, pos[1])
-            print(name, n1, n2)
-            nodes[n1].append((n2, in_or_out(pos[0], min_x, max_x, min_y, max_y)))
-            nodes[n2].append((n1, in_or_out(pos[1], min_x, max_x, min_y, max_y)))
+    portals = tuple((neighbor(nodes, pos), name + in_or_out(pos, min_x, max_x, min_y, max_y)) for pos, name in portals[0].items())
+
     return (nodes,
-            neighbor(nodes, portals[1]['AA'][0]),
-            neighbor(nodes, portals[1]['ZZ'][0]),
             portals)
 
-def bfs(graph, start, goal):
+def bfs(graph, start, goals):
+    results = dict()
     visited = dict()
-    to_explore = [((start, 0), 0, "")]
+    to_explore = [(start, 0)]
+    goals = dict(goals)
+
     while len(to_explore) > 0:
-        ((pos, depth), length, path) = to_explore[0]
-        if depth == 0 and pos == goal:
-            return (length, path)
+        (pos, cost) = to_explore[0]
         to_explore = to_explore[1::]
+        if not pos in visited:
+            # Skip Start -> Start by checking cost
+            if cost > 0 and pos in goals:
+                results[goals[pos]] = cost
+            visited[pos] = True
+            for (new_pos, new_cost) in graph[pos]:
+                if not new_pos in visited:
+                    to_explore.append((new_pos, cost + new_cost))
+    results = list(results.items())
+    results.sort(key=lambda g: g[1])
+    return results
+
+def bfs2(graph, start, goals):
+    # name, cost
+    results = dict()
+    visited = dict()
+    goals = dict(goals)
+    to_explore = [(start, 0, 0, start)]
+
+    while len(to_explore) > 0:
+        (pos, depth, cost, path) = to_explore[0]
+        to_explore = to_explore[1::]
+        if depth > 0 and pos in goals:
+            continue
         if not (pos, depth) in visited:
-            # print("Trying", pos, depth, path, graph[pos])
+            if depth == 0 and pos in goals:
+                results[pos] = (cost, path)
+                break
             visited[(pos, depth)] = True
-            for (new_pos, d) in graph[pos]:
-                if not (new_pos, depth) in visited:
-                    if d == 'I':
-                        depth = depth + 1
-                    elif d == 'O':
-                        depth = depth - 1
-                    if d != 'I' or depth > 0:
-                        to_explore.append(((new_pos, depth), length+1, path + d))
-    raise Exception("No path to goal")
-
-
+            for (new_pos, new_cost) in graph[pos]:
+                if not new_pos in visited:
+                    new_depth = 0
+                    if new_pos[0:2] == pos[0:2]:
+                        if new_pos[2] == 'O':
+                            new_depth = 1
+                        else:
+                            new_depth = -1
+                    if depth + new_depth >= 0 and depth < 1000:
+                        to_explore.append((new_pos, depth + new_depth, cost + new_cost, "%s->%s(%d)" % (path, new_pos, depth + new_depth)))
+    return results
 
 if len(sys.argv) != 2:
     raise Exception("Expected one argument")
 
-(graph, start, goal, portals) = parse_file(sys.argv[1])
+(graph, portals) = parse_file(sys.argv[1])
 
-# for y in range(38):
-    # for x in range(60):
-        # p = (x, y)
-        # if p in portals[0]:
-            # sys.stdout.write('*')
-        # elif p in graph:
-            # sys.stdout.write('.')
-        # else:
-            # sys.stdout.write(' ')
-    # print()
+dist = dict()
+for f in portals:
+    dist[f[1]] = bfs(graph, f[0], portals)
 
-# print(start, goal)
+# Add portal to portal edges
+for (pos, name) in portals:
+    if name[2] == 'I':
+        dist[name[0:2]+"I"].insert(0, (name[0:2] + "O", 1))
+        dist[name[0:2]+"O"].insert(0, (name[0:2] + "I", 1))
 
-(length, path) = bfs(graph, start, goal)
-
-print(length, path)
+print(bfs2(dist, "AAO", [("ZZO", 0)]))
